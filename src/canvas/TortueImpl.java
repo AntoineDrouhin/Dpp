9743e6700dcdb3ef6124706e26bb07b0695e0246
Antoine Drouhin <antoine.drouhin@gmail.com>
2017-06-13 15:09:00 +0200

    tortue init

---
+++ src/canvas/TortueImpl.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package canvas;
+
+/**
+ *
+ * @author Antoine
+ */
+public class TortueImpl implements Tortue {
+
+    public TortueImpl() {
+        this(400,200);
+    }
+    
+    public TortueImpl(int tX, int tY) {
+        // Canvas
+    }
+    
+    @Override
+    public void avancer(int dist) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void tourner(int angle) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
